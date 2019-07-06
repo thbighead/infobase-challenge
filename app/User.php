09ae88021df35a1f21ca6174339487f37ab4a63e
Thales Nathan <thcmatias@gmail.com>
2019-07-06 13:51:38 -0300

    Creating roles and permissions seeder and User policy
    User policy is already implemented, now we must implement the seeders so we can populate the database

--- app/User.php
+++ app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, SoftDeletes, HasRoles;
 
     /**
      * The attributes that are mass assignable.
